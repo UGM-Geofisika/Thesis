416942926352ca9edfdf0b8323b03ee1f7ed1460
UGM-Geofisika <ws@ugm.ac.id>
2017-10-04 19:42:23 +0700

    Add files via upload

---
+++ Jejak_Sinar_Seismic/hanya_contoh.m
@@ -0,0 +1,10 @@
+%% Hanya contoh skript MATLAB
+%%-----------------------------
+disp('------------------------------------------')
+disp('          Hanya Contoh Program Matlab')
+disp('------------------------------------------')
+
+A = 5;
+B = 6;
+
+disp(['                ' num2str(A) 'x' num2str(B) '=' num2str(A*B)])
